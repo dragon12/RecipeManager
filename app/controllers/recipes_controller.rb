9abc37ce52d02d2670fb6e12334418362829e456
Gerard Sweeney <gerard0sweeney@gmail.com>
2014-11-05 22:26:54 +0000

    better creation of stuff through associations

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,13 +12,15 @@ class RecipesController < ApplicationController
   end
   
   def update
+    render plain: params[:recipe].inspect
+    
     @recipe = Recipe.find(params[:id])
- 
-    if @recipe.update(recipe_params)
-      redirect_to @recipe
-    else
-      render 'edit'
-    end
+ @recipe.update(recipe_params)
+    #if @recipe.update(recipe_params)
+    #  redirect_to @recipe
+    #else
+     # render 'edit'
+    #end
   end
   
   def destroy
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,8 @@ class Recipe < ActiveRecord::Base
                     reject_if: lambda { |a| a[:quantity].blank? },
                     allow_destroy: true
                     
+  has_many :ingredients, through: :ingredient_quantities
+                    
   has_many :instructions
   accepts_nested_attributes_for :instructions, 
                     reject_if: lambda { |a| a[:details].blank? },
@@ -18,5 +20,31 @@ class Recipe < ActiveRecord::Base
   
   validates :name, presence: true,
                    length: { minimum: 5 }
-                    
+
+  def ingredient_quantities_attributes=(hash)
+    logger.info "setting ingredient quantities: #{hash.inspect}"
+    logger.info "this recipe id is #{self.id}"
+    
+    hash.each do |sequence, ingredient_quantity_values|
+      #find the ingredient in question
+      logger.info "this: #{ingredient_quantity_values}"
+      
+      i = Ingredient.find_or_create_by(name: ingredient_quantity_values[:quantity])
+      logger.info "i is now: #{i.inspect}"
+      #i = Ingredient.find_or_create_by(name: ingredient_quantity_values[:ingredient_name])
+      #for use in case of existing recipe: iq = IngredientQuantity.where()
+      # .. in which case we wouldn't add it to ingredient quantities directly
+      iq = IngredientQuantity.new
+      iq.ingredient = i
+      #iq.recipe = self
+      
+      ingredient_quantities << iq
+      logger.info "iq is now: #{iq.inspect}"
+     
+    end
+    
+  end
+  
+  private
+  
 end
