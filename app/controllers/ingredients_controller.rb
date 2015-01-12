fb613972b23bce34f20a5282bb3890f7915b601c
Gerard Sweeney <gerard0sweeney@gmail.com>
2015-01-12 22:08:27 +0000

    ingredient addition page

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,32 @@
 class IngredientsController < ApplicationController
-  def new
+  
+  def index
+    @ingredients = Ingredient.order(:name)
+  
+    @ingredient = Ingredient.new
+  end  
+  
+  def create
+    #render plain: params[:recipe].inspect
     
+    @ingredient = Ingredient.new(params.require(:ingredient).permit(:name))
+ 
+    if @ingredient.save
+      redirect_to action: "index"
+    else
+      @ingredients = Ingredient.order(:name)
+      render 'index'
+    end
+  end  
+  
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+    if @ingredient.destroy
+      redirect_to ingredients_path
+    else
+      @ingredients = Ingredient.order(:name)
+      render 'index'
+    end
+   
   end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,5 +3,17 @@ class Ingredient < ActiveRecord::Base
   has_many :recipes, through: :ingredient_quantities
   
   validates :name, presence: true,
-                   length: { minimum: 3 }
+                   length: { minimum: 3 },
+                   uniqueness: { case_sensitive: false }
+                   
+  before_destroy :check_for_recipes
+
+private
+
+  def check_for_recipes
+    if recipes.count > 0
+      errors.add(:base, "Cannot delete ingredient while present in any recipes")
+      return false
+    end
+  end
 end
