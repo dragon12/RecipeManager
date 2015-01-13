f28a0e190d170071e72fe85ea97440b9810fa03f
Gerard Sweeney <gerard0sweeney@gmail.com>
2015-01-13 20:45:58 +0000

    ingredient editing working

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -9,7 +9,7 @@ class IngredientsController < ApplicationController
   def create
     #render plain: params[:recipe].inspect
     
-    @ingredient = Ingredient.new(params.require(:ingredient).permit(:name))
+    @ingredient = Ingredient.new(ingredient_params)
  
     if @ingredient.save
       redirect_to action: "index"
@@ -17,16 +17,47 @@ class IngredientsController < ApplicationController
       @ingredients = Ingredient.order(:name)
       render 'index'
     end
-  end  
+  end
   
-  def destroy
+  def show
+    @ingredients = Ingredient.order(:name)
     @ingredient = Ingredient.find(params[:id])
-    if @ingredient.destroy
+    render 'index'
+  end
+  
+  def update
+#    render plain: params[:recipe].inspect
+    
+    @ingredient = Ingredient.find(params[:id])
+
+    if @ingredient.update(ingredient_params)
       redirect_to ingredients_path
     else
       @ingredients = Ingredient.order(:name)
       render 'index'
     end
+  end
+  
+  def destroy
+    @deleted_ingredient = Ingredient.find(params[:id])
+    if @deleted_ingredient.destroy
+      #redirect_to ingredients_path
+      message = @deleted_ingredient.name + " deleted successfully"
+      redirect_to ingredients_path, :notice => message
+    else
+      message = @deleted_ingredient.name + " couldn't be deleted: #{@deleted_ingredient.errors[:base].first.to_s}"
+      redirect_to ingredients_path, :alert => message
+      #@ingredients = Ingredient.order(:name)
+      #@ingredient = Ingredient.new
+      #render 'index'
+    end
    
   end
+  
+private
+
+  def ingredient_params
+    return params.require(:ingredient).permit(:name)
+  end
+  
 end
