7578959f7f8703a16f619f9f2287a7fdc763b790
Gerard Sweeney <gerard0sweeney@gmail.com>
2015-01-14 21:41:46 +0000

    about to start doing stuff with instructions

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -1,3 +1,9 @@
 class Instruction < ActiveRecord::Base
   belongs_to :recipe
+  
+  validates :step_number, presence: true
+  
+  validates :details, presence: true,
+                      length: {minimum: 3}
+                      
 end
