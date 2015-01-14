fa3ecc7842fca7600d0f297f43d77accf1364ef8
Gerard Sweeney <gerard0sweeney@gmail.com>
2015-01-14 22:12:31 +0000

    getting there with methods

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -1,7 +1,8 @@
 class Instruction < ActiveRecord::Base
   belongs_to :recipe
   
-  validates :step_number, presence: true
+  validates :step_number, presence: true,
+                          numericality: { only_integer: true }
   
   validates :details, presence: true,
                       length: {minimum: 3}
