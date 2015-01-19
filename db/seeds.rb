4464d67a5fc8a6e4256c17e289863bb15caf3ade
Gerard Sweeney <gerard0sweeney@gmail.com>
2015-01-19 21:21:43 +0000

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+salt = Ingredient.create(name: 'Salt')
+pepper = Ingredient.create(name: 'Pepper')
+chickpeas = Ingredient.create(name: 'Chickpeas')
+
+sampleRecipe = Recipe.new(name: 'Sample Recipe', description: 'This is a sample')
+
+sampleRecipe.ingredient_quantities.build(
+      quantity: '5 tsp', 
+      preparation: 'mixed',
+      ingredient: salt,
+      recipe: sampleRecipe)
+
+sampleRecipe.ingredient_quantities.build(
+  quantity: '500g', preparation: 'boiled', ingredient: chickpeas, recipe: sampleRecipe)
+
+#sampleRecipe.ingredient_quantities << iq1
+#sampleRecipe.ingredient_quantities << iq2
+
+sampleRecipe.instructions.build(step_number: 1, details: 'do the thing')
+
+sampleRecipe.save
