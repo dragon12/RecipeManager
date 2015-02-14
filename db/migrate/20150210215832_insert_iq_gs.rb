c40a9a67b3cc7199ae929c7cc551771717ab9fe4
Gerard Sweeney <gerard0sweeney@gmail.com>
2015-02-14 19:13:52 +0000

    fixed migration

--- db/migrate/20150210215832_insert_iq_gs.rb
+++ db/migrate/20150210215832_insert_iq_gs.rb
@@ -2,17 +2,20 @@ class InsertIqGs < ActiveRecord::Migration
   def change
     recipes = Recipe.all
 
+  IngredientQuantity.all.each do |iq|
+        iq.ingredient_quantity_group_id = iq.recipe_id
+        iq.save!
+    end
+      
     recipes.each do |r|
       iqg = IngredientQuantityGroup.new
       iqg.id = r.id
+      iqg.recipe_id = r.id
       iqg.name = "Ingredients"
-
-      iqg.save
-
-      r.ingredient_quantities.each do |iq|
-        iq.ingredient_quantity_group_id = iq.recipe_id
-        iq.save
-      end
+      
+      
+      iqg.save!
     end
+    
   end
 end
