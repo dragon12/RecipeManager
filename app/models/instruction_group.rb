0821a351f69f1d166a77d262839a26d892bc6a69
Gerard Sweeney <gerard0sweeney@gmail.com>
2015-02-15 13:41:32 +0000

    made db changes but as yet invisible

---
+++ app/models/instruction_group.rb
@@ -0,0 +1,23 @@
+class InstructionGroup < ActiveRecord::Base
+  before_destroy :check_for_instructions
+  
+  belongs_to :recipe
+  has_many :instructions, -> {order(:step_number)}, dependent: :destroy
+  
+  accepts_nested_attributes_for :instructions, allow_destroy: true
+  
+  validates_presence_of :instructions
+  validates :name, presence: true, length: {minimum: 2}
+  
+  def name_or_default
+    name.blank? ? "<Enter Group Name>" : name
+  end
+  
+private
+  def check_for_instructions
+    if instructions.count > 0
+      errors.add(:base, "Cannot delete instruction group while holding any instruction")
+      return false
+    end
+  end
+end
