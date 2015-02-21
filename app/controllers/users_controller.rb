9e2935bb481531fceba40d9a5285b9f0ef11e012
Gerard Sweeney <gerard0sweeney@gmail.com>
2015-02-21 22:59:03 +0000

    initial user login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to Recipes Managers!"
+      flash[:success] = "Welcome to Recipes Manager!"
+      log_in @user
       redirect_to @user
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   get 'categories/index'
@@ -30,6 +32,12 @@ Rails.application.routes.draw do
   resources :ingredients
   resources :categories
   resources :users
+  
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   # Example resource route with options:
   #   resources :products do
   #     member do
