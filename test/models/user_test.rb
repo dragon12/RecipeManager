1eb0cce80b1d63f4e532fdc82a0325ba6161bcaf
Gerard Sweeney <gerard0sweeney@gmail.com>
2015-02-28 09:44:33 +0000

    testing user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -38,4 +38,25 @@ class UserTest < ActiveSupport::TestCase
       assert @user.valid?, "#{valid_address.inspect} should be valid, got error: #{@user.errors.full_messages}"
     end
   end
+  
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+  
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 3
+    assert_not @user.valid?
+  end
+
+  test "authticate works with correct password" do
+    assert @user.authenticate("1234")
+  end
+
+
+  test "authticate fails with incorrect password" do
+    assert_not @user.authenticate("12345")
+  end
 end
