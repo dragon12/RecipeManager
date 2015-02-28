de091bc2f43f85ffde4adfa8ff0b222c9aa71eeb
Gerard Sweeney <gerard0sweeney@gmail.com>
2015-02-28 10:35:34 +0000

    some more users tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,10 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  setup do
-    @user = users(:one)
-  end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
+  def setup
+    @user       = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "should get new" do
@@ -16,34 +12,29 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should not create duplicate user" do
-    assert_no_difference('User.count') do
-      post :create, :user => @user.attributes
-    end
-
-    assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should show user" do
-    get :show, id: @user
-    assert_response :success
-  end
-
-  test "should get edit" do
+  test "should redirect edit when not logged in" do
     get :edit, id: @user
-    assert_response :success
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 
-  test "should update user" do
-    patch :update, id: @user, user: {  }
-    assert_redirected_to user_path(assigns(:user))
+  test "should redirect update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete :destroy, id: @user
-    end
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get :edit, id: @user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
 
-    assert_redirected_to users_path
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
