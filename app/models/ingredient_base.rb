2167de7f5677c27cc547ee64d122dc5a0a7e1a20
Gerard Sweeney <gerard0sweeney@gmail.com>
2015-05-04 17:38:10 +0100

    fixing base test

--- app/models/ingredient_base.rb
+++ app/models/ingredient_base.rb
@@ -1,2 +1,17 @@
 class IngredientBase < ActiveRecord::Base
+  has_many :ingredient_links
+  
+  validates :name, presence: true,
+                   length: { minimum: 3 },
+                   uniqueness: { case_sensitive: false }
+                   
+  before_destroy :check_for_ingredient_links
+  
+private
+  def check_for_ingredient_links
+    if ingredient_links.count > 0
+      errors.add(:base, "Cannot delete Ingredient Base while used in any Ingredient")
+      return false
+    end
+  end
 end
