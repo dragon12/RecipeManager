65d0331781b969d993f74a47302b738ab6e4da49
Gerard Sweeney <gerard0sweeney@gmail.com>
2015-05-04 17:29:26 +0100

    merged tests

--- test/models/ingredient_base_test.rb
+++ test/models/ingredient_base_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class IngredientBaseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @full_ing = ingredient_bases(:salt)
+  end
+  
+  test "starts as valid" do
+    i = @full_ing
+    assert i.valid?
+  end
+
+  test "name too short" do
+    i = @full_ing
+    i.name = "t"
+    assert_not i.valid?
+  end
+  
+  test "name unique" do
+    dup = @full_ing.dup
+    assert_not dup.valid?
+  end
+  
+  test "can't delete if linked ingredients" do
+    salt = ingredient_bases(:salt)
+    assert_no_difference 'IngredientBase.count' do
+      salt.destroy
+    end
+  end
+  
 end
